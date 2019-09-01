fed610c50606ed2f94109f390250d6f93a7ce058
Manish Dwivedi <dwivedimanish.ind@gmail.com>
2019-09-01 20:36:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
